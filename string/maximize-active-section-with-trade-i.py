c41432e229bcff8fbd4ab00c9b3e0b68cafb7e43
pranaygaira25 <pranaygaira25@gmail.com>
2026-07-21 20:31:12 +0530

    LC-maximize-active-section-with-trade-i [Runtime: N/A; Memory: N/A]

--- string/maximize-active-section-with-trade-i.py
+++ string/maximize-active-section-with-trade-i.py
@@ -1,25 +1,22 @@
 class Solution:
     def maxActiveSectionsAfterTrade(self, s: str) -> int:
-        ones = s.count('1')
+        ans = s.count('1')
+        pre = -10**9
+        mx = 0
+        i = 0
         n = len(s)
 
-        zero_blocks = []
-        one_blocks = []
-
-        i = 0
         while i < n:
             j = i
             while j < n and s[j] == s[i]:
                 j += 1
 
+            length = j - i
+
             if s[i] == '0':
-                zero_blocks.append(j - i)
-            else:
-                one_blocks.append(j - i)
+                mx = max(mx, pre + length)
+                pre = length
 
             i = j
 
-        if not zero_blocks or not one_blocks:
-            return ones
-
-        return ones + max(zero_blocks) - min(one_blocks)
+        return ans + mx
