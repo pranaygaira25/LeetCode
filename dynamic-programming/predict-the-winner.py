49c930d3fa7dd95a6cb9c70187e04cd9e183adea
pranaygaira25 <pranaygaira25@gmail.com>
2026-08-01 21:34:07 +0530

    LC-predict-the-winner [Runtime: N/A; Memory: N/A]

--- dynamic-programming/predict-the-winner.py
+++ dynamic-programming/predict-the-winner.py
@@ -1,7 +1,13 @@
-class Solution(object):
+class Solution:
     def predictTheWinner(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
-        
+        n = len(nums)
+        dp = nums[:]
+
+        for i in range(n - 2, -1, -1):
+            for j in range(i + 1, n):
+                dp[j] = max(
+                    nums[i] - dp[j],
+                    nums[j] - dp[j - 1]
+                )
+
+        return dp[-1] >= 0
