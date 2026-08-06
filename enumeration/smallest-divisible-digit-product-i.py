e542d2448189e4b1966d56198582593cc70f781b
pranaygaira25 <pranaygaira25@gmail.com>
2026-08-06 22:20:43 +0530

    LC-smallest-divisible-digit-product-i [Runtime: N/A; Memory: N/A]

---
+++ enumeration/smallest-divisible-digit-product-i.py
@@ -0,0 +1,11 @@
+class Solution:
+    def smallestNumber(self, n: int, t: int) -> int:
+        while True:
+            prod = 1
+            x = n
+            while x:
+                prod *= x % 10
+                x //= 10
+            if prod % t == 0:
+                return n
+            n += 1
