4952d3a1978976e51545dc3947fb79b9cb1a1368
pranaygaira25 <pranaygaira25@gmail.com>
2026-08-13 22:12:14 +0530

    LC-palindrome-number [Runtime: N/A; Memory: N/A]

--- math/palindrome-number.py
+++ math/palindrome-number.py
@@ -1,17 +1,14 @@
 class Solution(object):
     def isPalindrome(self, x):
-        """
-        :type x: int
-        :rtype: bool
-        """
-        # Negative numbers or numbers ending with 0 (but not 0 itself)
-        if x < 0 or (x % 10 == 0 and x != 0):
+        if x < 0:
             return False
-        
-        reversed_half = 0
-        
-        while x > reversed_half:
-            reversed_half = reversed_half * 10 + x % 10
+
+        original = x
+        reverse = 0
+
+        while x > 0:
+            digit = x % 10
+            reverse = reverse * 10 + digit
             x //= 10
-        
-        return x == reversed_half or x == reversed_half // 10
+
+        return original == reverse
