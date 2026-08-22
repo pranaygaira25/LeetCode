1a7e3d320a48716faa7479643fd30b47255e4e90
pranaygaira25 <pranaygaira25@gmail.com>
2026-08-23 00:09:43 +0530

    LC-check-divisibility-by-digit-sum-and-product [Runtime: N/A; Memory: N/A]

---
+++ math/check-divisibility-by-digit-sum-and-product.py
@@ -0,0 +1,13 @@
+class Solution:
+    def checkDivisibility(self, n: int) -> bool:
+        digit_sum = 0
+        digit_product = 1
+        num = n
+
+        while num > 0:
+            digit = num % 10
+            digit_sum += digit
+            digit_product *= digit
+            num //= 10
+
+        return n % (digit_sum + digit_product) == 0
