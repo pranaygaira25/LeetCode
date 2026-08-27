5e50624e459cee8919dddcae0302045567f48865
pranaygaira25 <pranaygaira25@gmail.com>
2026-08-27 17:05:50 +0530

    LC-lexicographically-smallest-permutation-greater-than-target [Runtime: N/A; Memory: N/A]

--- hash-table/lexicographically-smallest-permutation-greater-than-target.py
+++ hash-table/lexicographically-smallest-permutation-greater-than-target.py
@@ -4,7 +4,6 @@ class Solution:
         for ch in s:
             count[ord(ch) - ord('a')] += 1
         prefix = []
-        quinorath = (s, target)
         i = 0
         while i < len(target):
             x = ord(target[i]) - ord('a')
@@ -13,17 +12,19 @@ class Solution:
             count[x] -= 1
             prefix.append(target[i])
             i += 1
-        for j in range(i - 1, -1, -1):
-            used = ord(prefix.pop()) - ord('a')
-            count[used] += 1
+        for j in range(i, -1, -1):
+            if j < i:
+                used = ord(prefix.pop()) - ord('a')
+                count[used] += 1
             x = ord(target[j]) - ord('a')
             for c in range(x + 1, 26):
                 if count[c] > 0:
                     count[c] -= 1
-                    answer = prefix + [chr(c + ord('a'))]
+                    result = prefix + [chr(c + ord('a'))]
                     for k in range(26):
-                        answer.extend(
-                            [chr(k + ord('a'))] * count[k]
-                        )
-                    return "".join(answer)
+                        if count[k] > 0:
+                            result.append(
+                                chr(k + ord('a')) * count[k]
+                            )
+                    return "".join(result)
         return ""
