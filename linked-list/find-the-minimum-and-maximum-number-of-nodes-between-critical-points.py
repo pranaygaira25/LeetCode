ff0b65983513761d6bc2d3856447cef8f9f4931c
pranaygaira25 <pranaygaira25@gmail.com>
2026-09-01 00:17:30 +0530

    LC-find-the-minimum-and-maximum-number-of-nodes-between-critical-points [Runtime: N/A; Memory: N/A]

---
+++ linked-list/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
@@ -0,0 +1,31 @@
+class Solution:
+    def nodesBetweenCriticalPoints(self, head):
+        prev = head
+        curr = head.next
+        index = 1
+
+        first = -1
+        last = -1
+        min_dist = float('inf')
+        max_dist = 0
+
+        while curr.next:
+            if (curr.val > prev.val and curr.val > curr.next.val) or \
+               (curr.val < prev.val and curr.val < curr.next.val):
+
+                if first == -1:
+                    first = index
+                else:
+                    min_dist = min(min_dist, index - last)
+                    max_dist = index - first
+
+                last = index
+
+            prev = curr
+            curr = curr.next
+            index += 1
+
+        if first == last:
+            return [-1, -1]
+
+        return [min_dist, max_dist]
