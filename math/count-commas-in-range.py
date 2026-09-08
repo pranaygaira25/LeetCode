9b995e370ab8e68c3e0f3deac012affe3c7545ab
pranaygaira25 <pranaygaira25@gmail.com>
2026-09-08 23:53:42 +0530

    LC-count-commas-in-range [Runtime: N/A; Memory: N/A]

--- math/count-commas-in-range.py
+++ math/count-commas-in-range.py
@@ -1,18 +1,3 @@
 class Solution:
-    def countCommas(self, n: int, m: int) -> int:
-        ans = 0
-        start = 1000
-        commas = 1
-
-        while start <= m:
-            end = start * 1000 - 1
-            left = max(n, start)
-            right = min(m, end)
-
-            if left <= right:
-                ans += (right - left + 1) * commas
-
-            start *= 1000
-            commas += 1
-
-        return ans
+    def countCommas(self, n: int) -> int:
+        return max(0, n - 999)
