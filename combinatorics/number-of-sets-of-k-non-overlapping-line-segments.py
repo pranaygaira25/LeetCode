0c09d16931d838f9df5dd6f21195e99832786f4c
pranaygaira25 <pranaygaira25@gmail.com>
2026-09-16 22:36:14 +0530

    LC-number-of-sets-of-k-non-overlapping-line-segments [Runtime: N/A; Memory: N/A]

--- combinatorics/number-of-sets-of-k-non-overlapping-line-segments.py
+++ combinatorics/number-of-sets-of-k-non-overlapping-line-segments.py
@@ -1,13 +1,10 @@
 class Solution:
     def numberOfSets(self, n: int, k: int) -> int:
         MOD = 10**9 + 7
-        dp = [[0] * (k + 1) for _ in range(n)]
-
-        for i in range(n):
-            dp[i][0] = 1
+        dp = [1] * (k + 1)
 
         for i in range(1, n):
-            for j in range(1, min(k, i) + 1):
-                dp[i][j] = (dp[i - 1][j] + dp[i - 1][j - 1] + dp[i - 1][j]) % MOD
+            for j in range(1, k + 1):
+                dp[j] = (dp[j] + dp[j - 1]) % MOD
 
-        return dp[n - 1][k]
+        return dp[k]
