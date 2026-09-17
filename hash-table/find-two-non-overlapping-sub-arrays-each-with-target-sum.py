9872484a190bd829021ec1827fa7c4f7191590a1
pranaygaira25 <pranaygaira25@gmail.com>
2026-09-17 23:38:34 +0530

    LC-find-two-non-overlapping-sub-arrays-each-with-target-sum [Runtime: N/A; Memory: N/A]

--- hash-table/find-two-non-overlapping-sub-arrays-each-with-target-sum.py
+++ hash-table/find-two-non-overlapping-sub-arrays-each-with-target-sum.py
@@ -3,18 +3,29 @@ class Solution:
         n = len(arr)
         INF = n + 1
         best = [INF] * n
+        left = 0
+        total = 0
         ans = INF
-        prefix = 0
-        seen = {0: -1}
-        shortest = INF
-        for i in range(n):
-            prefix += arr[i]
-            if prefix - target in seen:
-                start = seen[prefix - target]
-                length = i - start + 1
-                if start >= 0:
-                    ans = min(ans, length + best[start - 1])
-                shortest = min(shortest, length)
-            best[i] = shortest
-            seen[prefix] = i
+
+        for right in range(n):
+            total += arr[right]
+
+            while total > target:
+                total -= arr[left]
+                left += 1
+
+            if total == target:
+                length = right - left + 1
+
+                if left > 0:
+                    ans = min(ans, length + best[left - 1])
+
+                if right > 0:
+                    best[right] = min(best[right - 1], length)
+                else:
+                    best[right] = length
+            else:
+                if right > 0:
+                    best[right] = best[right - 1]
+
         return -1 if ans == INF else ans
